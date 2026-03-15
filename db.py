fdf7208386b5ebc0907a1770cacad30855f27b45
tladkins <tladkins@cincinnatistate.edu>
2026-03-14 22:25:17 -0400

    Update db.py

--- db.py
+++ db.py
@@ -11,7 +11,8 @@ def get_connection():
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
+        port=os.getenv("DB_PORT"),
+        sslmode='require'
     )
 
 
